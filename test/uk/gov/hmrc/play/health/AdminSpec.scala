d2ec290035db3b0aabb2e8b26b1e45e9808ed537
Charles Kubicek <ckubicek@equalexperts.com>
2014-07-14 13:55:48 +0100

    HMTB-4163 Charles,Mark refactoring

--- test/uk/gov/hmrc/play/health/AdminSpec.scala
+++ test/uk/gov/hmrc/play/health/AdminSpec.scala
@@ -1,42 +1,45 @@
 package uk.gov.hmrc.play.health
 
 import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
-import org.scalatest.{ShouldMatchers, FunSpec}
+import org.scalatest.{WordSpec, ShouldMatchers, FunSpec}
 import play.api.test._
 import play.api.libs.ws.{Response, WS}
 
-class AdminSpec extends FunSpec with ShouldMatchers with PlayRunners with ScalaFutures with DefaultAwaitTimeout with IntegrationPatience {
+class AdminSpec extends WordSpec with ShouldMatchers with PlayRunners with ScalaFutures with DefaultAwaitTimeout with IntegrationPatience {
 
-  describe("Ping endpoint") {
-    it("should respond with a 200 status code when the service is OK") {
-      running(TestServer(3333)) {
-        WS.url("http://localhost:3333/ping/ping").get.futureValue.status shouldBe 200
-      }
+  trait Resource {
+    this: WithServer =>
+    def resource(path: String) = WS.url(s"http://localhost:$port" + path).get().futureValue
+  }
+
+  abstract class ServerWithConfig(conf: Map[String, String] = Map.empty) extends
+    WithServer(FakeApplication(additionalConfiguration = conf)) with Resource
+
+
+  "The Ping endpoint" should {
+    "respond with a 200 status code when the service is OK" in new ServerWithConfig {
+      resource("/ping/ping").status shouldBe 200
     }
   }
 
-  describe("details endpoint") {
-    it("should respond with a 200 when service is configured with an appName") {
-      running(TestServer(3333, new FakeApplication(additionalConfiguration = Map("appName" -> "play-health")))) {
-        WS.url("http://localhost:3333/admin/details").get.futureValue.status shouldBe 200
+  "The details endpoint" should {
+    "respond with a 200 when service is configured with an appName" in
+      new ServerWithConfig(Map("appName" -> "play-health")) {
+        resource("/admin/details").status shouldBe 200
       }
-    }
 
-    it("should respond with a 500 status code when an 'appName' value is not present in the config") {
-      running(TestServer(3333, new FakeApplication(additionalConfiguration = Map()))) {
-        WS.url("http://localhost:3333/admin/details").get.futureValue.status shouldBe 500
+    "respond with a 500 status code when an 'appName' value is not present in the config" in
+      new ServerWithConfig(Map.empty) {
+        resource("/admin/details").status shouldBe 500
       }
-    }
-  }
 
-  describe("configuration endpoint") {
-    it("should respond with a 200 and the Play config") {
-      running(TestServer(3333, new FakeApplication(additionalConfiguration = Map("configKey" -> "value")))) {
-        val response = WS.url("http://localhost:3333/admin/conf").get.futureValue
+    "The configuration endpoint" should {
+      "respond with a 200 and the Play config" in
+        new ServerWithConfig(Map("configKey" -> "value")) {
+          val response = resource("/admin/conf")
 
-        response.status shouldBe 200
-        response.body should include( """"configKey" : "value"""")
-      }
+          response.status shouldBe 200
+          response.body should include( """"configKey" : "value"""")
+        }
     }
   }
-}
