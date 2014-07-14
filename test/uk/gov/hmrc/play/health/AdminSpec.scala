b195483da26408d78bffc868d481cd24db41082d
Charles Kubicek <ckubicek@equalexperts.com>
2014-07-14 12:01:59 +0100

    HMTB-4163 Charles,Mark refactoring

--- test/uk/gov/hmrc/play/health/AdminSpec.scala
+++ test/uk/gov/hmrc/play/health/AdminSpec.scala
@@ -5,15 +5,17 @@ import org.scalatest.{ShouldMatchers, FunSpec}
 import play.api.test._
 import play.api.libs.ws.WS
 
-class HealthSpec extends FunSpec with ShouldMatchers with PlayRunners with ScalaFutures with DefaultAwaitTimeout with IntegrationPatience {
+class AdminSpec extends FunSpec with ShouldMatchers with PlayRunners with ScalaFutures with DefaultAwaitTimeout with IntegrationPatience {
 
-  describe("Health endpoint") {
+  describe("Ping endpoint") {
     it("should respond with a 200 status code when the service is OK") {
       running(TestServer(3333)) {
         WS.url("http://localhost:3333/ping/ping").get.futureValue.status shouldBe 200
       }
     }
+  }
 
+  describe("details endpoint") {
     it("should respond with a 200 when service is configured with an appName") {
       running(TestServer(3333, new FakeApplication(additionalConfiguration = Map("appName" -> "play-health")))) {
         WS.url("http://localhost:3333/ping/details").get.futureValue.status shouldBe 200
