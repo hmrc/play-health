f041c050ea067109aea4c985e99a6983682016d3
Duncan Crawford <duncan@tandemconsulting.eu>
2014-07-29 11:14:33 +0100

    increment play to 2.3.2 and scala to 2.11.2

--- project/HmrcBuild.scala
+++ project/HmrcBuild.scala
@@ -1,32 +1,44 @@
 import sbt._
 import Keys._
-import play.Project._
 
 object HmrcBuild extends Build {
 
   import uk.gov.hmrc.DefaultBuildSettings
+  import DefaultBuildSettings._
+  import BuildDependencies._
+  import uk.gov.hmrc.{SbtBuildInfo, ShellPrompt}
+  import play.core.PlayVersion
+  import play.PlayImport._
 
   val nameApp = "play-health"
-  val versionApp = "0.2.0"
+  val versionApp = "0.3.0"
 
   val appDependencies = Seq(
-    "org.scalatest" %% "scalatest" % "2.2.0",
-    "com.typesafe.play" %% "play" % "2.2.3",
-    "org.pegdown" % "pegdown" % "1.4.2"
+    "com.typesafe.play" %% "play" % PlayVersion.current,
+    ws % "provided",
+    "org.scalatest" %% "scalatest" % "2.2.0" % "test",
+    "org.pegdown" % "pegdown" % "1.4.2" % "test"
   )
 
-  lazy val root = Project(nameApp, file("."), settings = DefaultBuildSettings(nameApp, versionApp, targetJvm = "jvm-1.7")()
-    ++ playScalaSettings
-    ++ Seq(
+  lazy val playHealth = Project(nameApp, file("."))
+    .enablePlugins(play.PlayScala)
+    .settings(version := versionApp)
+    .settings(scalaSettings : _*)
+    .settings(defaultSettings() : _*)
+    .settings(
+      targetJvm := "jvm-1.7",
+      shellPrompt := ShellPrompt(versionApp),
       libraryDependencies ++= appDependencies,
       resolvers := Seq(
         Opts.resolver.sonatypeReleases,
         Opts.resolver.sonatypeSnapshots,
-        "typesafe-releases"  at "http://repo.typesafe.com/typesafe/releases/",
-        "typesafe-snapshots" at "http://repo.typesafe.com/typesafe/snapshots/"
-      )
-  ) ++ SonatypeBuild()
-  )
+      "typesafe-releases" at "http://repo.typesafe.com/typesafe/releases/",
+      "typesafe-snapshots" at "http://repo.typesafe.com/typesafe/snapshots/"
+      ),
+      crossScalaVersions := Seq("2.11.2", "2.10.4")
+    )
+    .settings(SbtBuildInfo(): _*)
+    .settings(SonatypeBuild(): _*)
 }
 
 object SonatypeBuild {
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,7 +1,7 @@
-addSbtPlugin("uk.gov.hmrc" % "sbt-utils" % "1.2.0")
+addSbtPlugin("uk.gov.hmrc" % "sbt-utils" % "2.0.2")
 
 addSbtPlugin("org.xerial.sbt" % "sbt-sonatype" % "0.2.1")
 
 addSbtPlugin("com.typesafe.sbt" % "sbt-pgp" % "0.8.1")
 
-addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.2.3")
+addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.3.2")
