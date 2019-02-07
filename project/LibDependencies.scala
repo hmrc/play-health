15988ec091163ba56ee61e6cdd0d52c8fb4afb13
Tomasz Rosiek <7059542+tomasz-rosiek@users.noreply.github.com>
2019-02-07 17:06:28 +0000

    PLATOPS-1850 Updated library versions to solve the problem of security vulnerabilities

--- project/LibDependencies.scala
+++ project/LibDependencies.scala
@@ -5,7 +5,15 @@ import sbt._
 object LibDependencies {
 
   val compile: Seq[ModuleID] = Seq(
-    "com.typesafe.play" %% "play" % PlayVersion.current
+    "com.typesafe.play" %% "play" % PlayVersion.current,
+    // force dependencies due to security flaws found in jackson-databind < 2.9.x using XRay
+    "com.fasterxml.jackson.core"     % "jackson-core"            % "2.9.7",
+    "com.fasterxml.jackson.core"     % "jackson-databind"        % "2.9.7",
+    "com.fasterxml.jackson.core"     % "jackson-annotations"     % "2.9.7",
+    "com.fasterxml.jackson.datatype" % "jackson-datatype-jdk8"   % "2.9.7",
+    "com.fasterxml.jackson.datatype" % "jackson-datatype-jsr310" % "2.9.7",
+    // force dependencies due to security flaws found in xercesImpl 2.11.0
+    "xerces" % "xercesImpl" % "2.12.0"
   )
 
   val test: Seq[ModuleID] = dependencies(
